1e4eeab3f888e191962d65b7753fc800bf684e8a
linsen <614793218@qq.com>
2022-12-03 22:02:42 +0800

    123

--- src/main/java/com/ls/Main.java
+++ src/main/java/com/ls/Main.java
@@ -2,11 +2,12 @@ package com.ls;
 
 /**
  * @author LS
- * @date ${DATE} ${TIME}
- * @priject ${PROJECT_NAME}
  **/
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/com/ls/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.ls.controller;
+
+/**
+ * @author LS
+ * @datetime 2022/12/3 21:59
+ * @project mytest
+ **/
+public class TestController {
+}
