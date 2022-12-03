82dc4d3a477ff48f2c7bb4a1b4478fe962fb6225
linsen <614793218@qq.com>
2022-12-03 22:27:06 +0800

    dev

--- src/main/java/com/ls/Main.java
+++ src/main/java/com/ls/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("dev提交!");
     }
 }
